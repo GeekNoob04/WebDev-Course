09f5d7787ec1bf339fe20334323ba4000500797c
GeekNoob04 <harshitbudhraja0@gmail.com>
2024-02-03 15:01:03 +0530

    initial commit

---
+++ video58/index.js
@@ -0,0 +1,20 @@
+
+function nice(name){
+    console.log("Hey "+ name + " you are nice")
+    console.log("Hey "+ name + " you are good")
+    console.log("Hey "+ name + " your tshirt is nice")
+    console.log("Hey "+ name + " your course is good too")
+}
+nice("Harshit")
+
+function sum(a,b){
+    // console.log(a+b)
+    return a+b 
+}
+result =sum(3,5)
+console.log("The sum is:",result)
+
+const func1 = (x)=>{
+    console.log("I am an arrow function",x)
+}
+func1(34) 
